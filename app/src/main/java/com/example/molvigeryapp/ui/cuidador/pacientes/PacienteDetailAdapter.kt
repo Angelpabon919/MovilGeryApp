69d7c38134f38622f4ac863b99f894b864dd9d1b
Jeison Burbano <pastasburbano204@gmail.com>
2026-09-12 09:14:12 -0500

    se sube el evento adverso y la barra de navegacion y se agrega el cerrar sesion

--- app/src/main/java/com/example/molvigeryapp/ui/cuidador/pacientes/PacienteDetailAdapter.kt
+++ app/src/main/java/com/example/molvigeryapp/ui/cuidador/pacientes/PacienteDetailAdapter.kt
@@ -2,10 +2,11 @@ package com.example.molvigeryapp.ui.cuidador.pacientes
 
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.molvigeryapp.ui.cuidador.bitacora.EventosAdversosFragment
 
 class PacienteDetailAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
-    override fun getItemCount(): Int = 5
+    override fun getItemCount(): Int = 6
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
@@ -14,6 +15,7 @@ class PacienteDetailAdapter(fragment: Fragment) : FragmentStateAdapter(fragment)
             2 -> CardexFragment()
             3 -> RecomendacionesFragment()
             4 -> InsumosFragment()
+            5 -> EventosAdversosFragment()
             else -> DatosBasicosFragment()
         }
     }
